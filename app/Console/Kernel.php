193667908c233f96667acd13a815f89e05fa0487
hokton-bd <skmhkt.p@gmail.com>
2022-08-25 20:07:00 +0900

    edit kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('scrape:run')->dailyAt('11:00');
+        $schedule->command('scrape:run')->dailyAt('13:00');
+        // $schedule->command('scrape:run')->everyMinute();
     }
 
     /**
